5524a9ec2a78437229417e462716160a51ef9b15
Cristian Varela Casas <cristianchantada@gmailcom>
2024-08-12 19:09:27 +0200

    Realizada ruta subir imagenes

---
+++ controllers/uploads.js
@@ -0,0 +1,72 @@
+const { response } = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const fileUpload = (req, res = response) => {
+  const tipo = req.params.tipo;
+  const id = req.params.id;
+
+  const tiposValidos = ["hospitales", "medicos", "usuarios"];
+
+  if (!tiposValidos.includes(tipo)) {
+    return res.status(400).json({
+      ok: false,
+      msg: "No es un médico, usuario u hospital (tipo)",
+    });
+  }
+
+  // Validar que exista un archivo
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({
+      ok: false,
+      msg: "No hay ningún archivo",
+    });
+  }
+
+  // Procesar la imagen...
+
+  const file = req.files.imagen;
+
+  const nombreCortado = file.name.split(".");
+  const extensionArchivo = nombreCortado[nombreCortado.length - 1];
+
+  // Validar extansión
+  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
+
+  if (!extensionesValidas.includes(extensionArchivo)) {
+    return res.status(400).json({
+      ok: false,
+      msg: "No es una extensión permitida",
+    });
+  }
+
+  // Generar el nombre del archivo
+  const nombreArchivo = `${uuidv4()}.${extensionArchivo}`;
+
+  // Path para guardar la imagen
+  const path = `./uploads/${tipo}/${nombreArchivo}`;
+  console.log({path});
+  
+
+  // Mover la imagen
+  file.mv(path, (err) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        msg: 'Error al mover la imagen'
+      });
+    }
+
+    res.json({
+      ok: true,
+      msg: 'Archivo subido',
+      nombreArchivo
+    });
+
+  });
+
+
+};
+
+module.exports = {
+  fileUpload,
+};
---
+++ routes/uploads.js
@@ -0,0 +1,14 @@
+/**
+ * Ruta: api/uploads/
+ */
+
+const { Router } = require('express');
+const expressfileUpload = require('express-fileupload');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { fileUpload } = require('../controllers/uploads')
+const router = Router();
+
+router.use(expressfileUpload());
+router.put('/:tipo/:id', validarJWT, fileUpload);
+
+module.exports = router;
