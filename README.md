a00ed4dcb3288fd4e906baa58a4c194532268eee
cvarela <cristianchantada@gmail.com>
2024-08-12 12:37:45 +0200

    Fin sección 10 auth

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 "# backend-server-angular-avanzado" 
+
+```
+npm install
+```
+
+```
+npm tun start:dev
+```
+
+
--- index.js
+++ index.js
@@ -10,16 +10,19 @@ const app = express();
 // CORS
 app.use(cors());
 
+// Lectura y parseo del body
+app.use( express.json());
+
 // Base de datos
 dbConnection();
 
 // Rutas
-app.get('/', (req, res) => {
-    res.status(400).json({
-      ok: true,
-      msg: 'Hola Mundo !'
-    })
-});
+
+
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
+
+
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto', process.env.PORT);
