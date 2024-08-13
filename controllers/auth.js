e4fbc2d96e3528f1f6f8f1788d9f7ade4f84a085
cvarela <cristianchantada@gmail.com>
2024-08-13 11:09:54 +0200

    Fin sección 12

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,55 +1,97 @@
-const { response } = require('express');
-const Usuario = require('../models/usuario');
-const bcrypt = require('bcryptjs');
-const { generarJWT } = require('../helpers/jwt')
+const { response } = require("express");
+const Usuario = require("../models/usuario");
+const bcrypt = require("bcryptjs");
+const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
-const login = async (req, res) => {
-  
+const login = async (req, res = response) => {
   const { email, password } = req.body;
-  
+
   try {
-
     // Verificar email
-    const usuarioDB = await Usuario.findOne({email});
+    const usuarioDB = await Usuario.findOne({ email });
 
-    if(!usuarioDB){
+    if (!usuarioDB) {
       return res.status(404).json({
         ok: false,
-        msg: 'Email no válido'
+        msg: "Email no válido",
       });
     }
 
     // Verificar contraseña
 
     const validPassword = bcrypt.compareSync(password, usuarioDB.password);
-    
-    if(!validPassword) {
+
+    if (!validPassword) {
       return res.status(400).json({
         res: false,
-        msg: 'Contraseña no válida',
-      })
+        msg: "Contraseña no válida",
+      });
     }
 
     // Generar el JWToken
-
     const token = await generarJWT(usuarioDB.id);
 
     res.json({
       ok: true,
-      token
-    })
-
+      token,
+    });
   } catch (error) {
-
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: 'Hable con el administrasdor',
-    })
-    
+      msg: "Hable con el administrasdor",
+    });
   }
-}
+};
+
+const googleSignIn = async (req, res = response) => {
+  try {
+    const { email, name, picture} = await googleVerify(req.body.token);
+
+    const usuarioDB = await Usuario.findOne({email});
+    let usuario;
+
+    if(!usuarioDB){
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: '@@@',
+        img: picture,
+        google: true,
+      })
+    } else {
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    await usuario.save();
+    
+    // Generar el JWToken
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      email, name, picture,
+      token,
+    })
+
+  } catch (error) {
+    console.log({error});
+    res.status(400).json({
+      ok: true,
+      msg: 'El token de Google no es correcto',
+    })
+  }
+
+
+  res.json({
+    ok: true,
+    msg: req.body.token,
+  });
+};
 
 module.exports = {
   login,
-}
+  googleSignIn,
+};
